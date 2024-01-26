8be8e2329bc71c32a65b93186ae25199e12b17a3
GevChalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-01-25 21:09:47 -0800

    Initial Commit

---
+++ 1.two-sum.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+#include <vector>
+#include <unordered_map>
+
+// @lc code=start
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        unsigned int numsSize = nums.size();
+        std::unordered_map<int, int> map;
+
+        for (int i = 0; i < numsSize; i++) {
+            int complement = target - nums[i];
+
+            if(map.find(complement) != map.end())
+                return { map.at(complement), i };
+
+            map.insert(nums[i], i);
+        }
+    }
+};
+// @lc code=end
+
