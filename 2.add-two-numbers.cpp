db5ffbcb1f4b6602b3da863675c3e79d472a32ee
GevChalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-01-30 13:31:15 -0800

    2.add-two-numbers.cpp Accepted
    - 1568/1568 cases passed (20 ms)
    - Runtime beats 70.5 % of cpp submissions
    - Memory usage beats 15.92 % of cpp submissions (75.5 MB)

---
+++ 2.add-two-numbers.cpp
@@ -0,0 +1,58 @@
+/*
+ * @lc app=leetcode id=2 lang=cpp
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = l1;
+        bool carry = false;
+
+        l1->val += l2->val + (carry * 1);
+        carry = l1->val >= 10;
+        l1->val -= (10 * carry);
+
+        while (l2->next != nullptr && l1->next != nullptr) {
+            l1->next->val += l2->next->val + (carry * 1);
+
+            carry = l1->next->val >= 10;
+            l1->next->val -= (10 * carry);
+
+            l1 = l1->next;
+            l2 = l2->next;
+        }
+
+        l1->next = l1->next == nullptr ? l2->next : l1->next;
+
+        while (carry && l1->next != nullptr) {
+            l1->next->val += (carry * 1);
+
+            carry = l1->next->val >= 10;
+            l1->next->val -= (10 * carry);
+
+            l1 = l1->next;
+        }
+
+        if (carry) {
+            l1->next = new ListNode(1);
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
+
