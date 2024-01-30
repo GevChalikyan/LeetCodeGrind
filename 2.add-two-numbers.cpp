419c62f484d6f2a0cb8664bf0f213ba3447fb22b
GevChalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-01-30 13:49:00 -0800

    Added a conditional definition for ListNode
    - Prevents error squiggles in local editor
    - Doesn't break LeetCode submission
    - (Maybe?) Enables local testing — UNTESTED
    - Adding comments for readability

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -16,11 +16,23 @@
  * };
  */
 
+#ifndef LISTNODE_H
+
+struct ListNode {     
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+#endif
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = l1;
-        bool carry = false;
+        ListNode* ans = l1;     //  Preserve initial l1 pointer
+        bool carry = false;     //  
 
         l1->val += l2->val + (carry * 1);
         carry = l1->val >= 10;
