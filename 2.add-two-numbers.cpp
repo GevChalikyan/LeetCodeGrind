1f3d86b1db7d12de3c0bad88c9283f0541cf77ad
GevChalikyan <72428068+GevChalikyan@users.noreply.github.com>
2024-02-02 00:27:38 -0800

    Added empty function for practice - Left last attempt to see improvement - Added more readable version of answer, with the help of copilot, for reference

--- 2.add-two-numbers.cpp
+++ 2.add-two-numbers.cpp
@@ -24,13 +24,14 @@ struct ListNode {
     ListNode() : val(0), next(nullptr) {}
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
+};
 
 #endif
 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+	//	Attempt #1	
+    /*ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* ans = l1;     //  Preserve initial l1 pointer
         bool carry = false;     //  
 
@@ -64,7 +65,37 @@ public:
         }
 
         return ans;
-    }
+    }*/
+
+	//	Reference Answer
+	/*ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int* throwaway) {
+		ListNode* ans = l1;
+		bool carry = false;
+
+		l1->val += l2->val;
+		carry = l1->val >= 10;
+		l1->val -= (10 * carry);
+
+		while (l2->next != nullptr || l1->next != nullptr || carry) {
+			if (l1->next == nullptr && (l2->next != nullptr || carry)) {
+				l1->next = new ListNode(0);
+			}
+
+			l1 = l1->next;
+			l2 = l2->next != nullptr ? l2->next : new ListNode(0);
+
+			l1->val += l2->val + carry;
+			carry = l1->val >= 10;
+			l1->val -= (10 * carry);
+		}
+
+		return ans;
+	}*/
+
+	//	Practice
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int* throwaway) {
+		return nullptr;
+	}
 };
 // @lc code=end
 
